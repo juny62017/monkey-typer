6a08c06c2006e7dceb0eb7a5760279b8378d427f
juny62017 <juny62017@gmail.com>
2026-04-29 03:21:20 +0530

    made css And script

---
+++ script.js
@@ -0,0 +1,10 @@
+let startBtn = document.getElementById("startBtn");
+let resetBtn = document.getElementById("resetBtn");
+
+startBtn.onclick = function () {
+    alert("Test will start in next update");
+};
+
+resetBtn.onclick = function () {
+    alert("Reset will be added later");
+};
